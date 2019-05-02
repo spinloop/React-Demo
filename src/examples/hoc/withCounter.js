8e0ed561c5c089c777de2e1f2b117b0ef209826b
spinloop <spinloop@gmail.com>
2019-05-02 12:52:28 -0400

    [hoc] add common functionality to hoc and expose using props

--- src/examples/hoc/withCounter.js
+++ src/examples/hoc/withCounter.js
@@ -2,9 +2,26 @@ import React from 'react'
 
 const withCounter = (WrappedComponent) => {
   class WithCounter extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        count: 0
+      }
+    }
+
+    increment = () => {
+      this.setState(prevState => {
+        return { count: prevState.count + 1 }
+      })
+    }
+
     render() {
       return (
-        <WrappedComponent {...this.props} />
+        <WrappedComponent
+          count={this.state.count}
+          increment={this.increment}
+          {...this.props}
+        />
       )
     }
   }
