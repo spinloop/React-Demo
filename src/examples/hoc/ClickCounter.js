cefeb340e0510bdf09531bfc5f9f876388b1dd54
spinloop <spinloop@gmail.com>
2019-05-02 13:23:14 -0400

    refactor to use arrow function

--- src/examples/hoc/ClickCounter.js
+++ src/examples/hoc/ClickCounter.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import withCounter from './withCounter'
 
-class ClickCounter extends React.Component {
-  render() {
-    const { count, increment, type } = this.props
-
-    return (
-      <button onClick={increment}>{`${count} button ${type}s`}</button>
-    )
-  }
-}
+const ClickCounter = ({ count, increment, type }) => (
+  <button onClick={increment}>{`${count} button ${type}s`}</button>
+)
 
 export default withCounter(ClickCounter, 1)
--- src/examples/hoc/HoverCounter.js
+++ src/examples/hoc/HoverCounter.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import withCounter from './withCounter'
 
-class HoverCounter extends React.Component {
-  render() {
-    const { count, increment, type } = this.props
-
-    return (
-      <button onMouseOver={increment}>{`${count} button ${type}s`}</button>
-    )
-  }
-}
+const HoverCounter = ({ count, increment, type }) => (
+  <button onMouseOver={increment}>{`${count} button ${type}s`}</button>
+)
 
 export default withCounter(HoverCounter, 10)
