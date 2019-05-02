526ac90035599d6c9978af7853d51c546c66ec83
spinloop <spinloop@gmail.com>
2019-05-02 15:13:16 -0400

    [hooks] declare new state variable and set initial value

--- src/examples/hooks/index.js
+++ src/examples/hooks/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../App.css';
 
 export default () => {
+  const [count, setCount] = useState(0)
+
   return (
     <div className="Example">
       <header className="Example-header">Hook Demo</header>
