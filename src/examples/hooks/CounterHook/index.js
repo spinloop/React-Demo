1e7f2805a9ba5bf60e9b49fbb7b94f2f6dff9278
spinloop <spinloop@gmail.com>
2019-05-07 11:37:30 -0400

    initial component (before extracting custom hook)

--- src/examples/hooks/CounterHook/index.js
+++ src/examples/hooks/CounterHook/index.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const CounterHookExample = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    console.log('count: ' + count)
+
+    if (count >= 10) {
+      // clean up example that keeps counter in single digits
+      return setCount(0)
+    }
+  }, [count])
+
   return ([
     <header className="Example-header">Counter Hook Example</header>,
+    <div>
+      <button onClick={() => setCount(count + 1)}>the count is {count}</button>
+    </div>
   ])
 }
 
