2bb9e87bb0e52e5e3c2b7e0f2abc203f2f37ae3d
spinloop <spinloop@gmail.com>
2019-05-07 11:17:14 -0400

    example setup

---
+++ src/examples/hooks/CounterHook/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CounterHookExample = () => {
+  return ([
+    <header className="Example-header">Counter Hook Example</header>,
+  ])
+}
+
+export default CounterHookExample
